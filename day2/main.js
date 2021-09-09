73c0d2c453bf24d2bc33d871b4c5913f038ae23c
arifincesar <arifincaesar@gmail.com>
2021-09-09 19:18:46 +0700

    squential algoritma

---
+++ day2/main.js
@@ -0,0 +1,13 @@
+//konsep squential
+//program: menghitung luas segitiga
+//rumus luas segitiga = (alas*tinggi) / 2
+
+//input
+const alas = 10;
+const tinggi = 20;
+
+//proses
+const luassegitiga = (alas * tinggi) / 2;
+
+//output
+console.log(luassegitiga);
